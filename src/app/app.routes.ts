7a34ab4fba4ad24bc20d943cd4edf8bec7010df3
gildrausNV <milos.vujcic.997@gmail.com>
2025-07-19 18:59:05 +0200

    invitation page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './domains/home/home.component';
 import { FeedbackFormComponent } from './domains/feedback-form/feedback-form.component';
 import { SignUpFormComponent } from './domains/sign-up-form/sign-up-form.component';
+import { InvitationComponent } from './domains/invitation/invitation.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +17,13 @@ export const routes: Routes = [
     {
         path: 'signup',
         component: SignUpFormComponent
+    },
+    {
+        path: 'invitation',
+        component: InvitationComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
---
+++ src/app/domains/invitation/invitation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-invitation',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './invitation.component.html',
+  styleUrl: './invitation.component.scss'
+})
+export class InvitationComponent {
+
+}
