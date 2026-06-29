01f6e6996db3bdd7c67b3d1a4622f5ddeb0e2ad7
gildraus <milos.vujcic.997@gmail.com>
2026-06-29 22:46:45 +0200

    Brought back old ui, changed color scheme

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,53 +1,53 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  days: number = 0;
-  hours: number = 0;
-  minutes: number = 0;
-  seconds: number = 0;
-  
-  private intervalId: any;
-  private targetDate: Date = new Date('2026-04-20T00:00:00');
+export class HomeComponent {
 
-  ngOnInit(): void {
-    this.updateCountdown();
-    this.intervalId = setInterval(() => {
-      this.updateCountdown();
-    }, 1000);
-  }
 
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
 
-  private updateCountdown(): void {
-    const now = new Date().getTime();
-    const distance = this.targetDate.getTime() - now;
 
-    if (distance > 0) {
-      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    } else {
-      this.days = 0;
-      this.hours = 0;
-      this.minutes = 0;
-      this.seconds = 0;
-    }
-  }
 
-  padZero(num: number): string {
-    return num < 10 ? '0' + num : num.toString();
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
