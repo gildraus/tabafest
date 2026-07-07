c83936a7a9d96ef46beb0cf2bb889094fa7ccfcc
gildraus <milos.vujcic.997@gmail.com>
2026-07-08 00:18:50 +0200

    redesign

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  @ViewChild('heroVideo') heroVideoRef!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    this.safePlay();
+  }
+
+  onCanPlay() {
+    this.safePlay();
+  }
+
+  private safePlay() {
+    const video = this.heroVideoRef?.nativeElement;
+    if (!video) {
+      return;
+    }
+
+    video.defaultMuted = true;
+    video.muted = true;
+    video.volume = 0;
+    video.playsInline = true;
+    video.play().catch(() => {});
+  }
+
 
 
 
